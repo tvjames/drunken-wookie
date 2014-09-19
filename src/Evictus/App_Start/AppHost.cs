9d6bfe19991d3792a2bd5e523679cb418d3b979e
Thomas James <thomasvjames@gmail.com>
2014-09-19 10:52:11 +1000

    Bare bones

---
+++ src/Evictus/App_Start/AppHost.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using ServiceStack;
+using ServiceStack.Logging.NLogger;
+using ServiceStack.Razor;
+using ServiceStack.Validation;
+
+namespace DrunkenWookie.Evictus
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Evictus - Drunken Wookie", typeof(AppHost).Assembly)
+        {
+        }
+
+        public static void Start()
+        {
+            NLog.LogManager.GetCurrentClassLogger().Info("Start");
+
+            new AppHost().Init();
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+            ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
+            ServiceStack.Text.JsConfig.IncludeNullValues = true;
+            ServiceStack.Logging.LogManager.LogFactory = new NLogFactory();
+
+            Plugins.Add(new RazorFormat { LoadFromAssemblies = new List<Assembly> { typeof(AppHost).Assembly } });
+            Plugins.Add(new ValidationFeature());
+
+            container.RegisterValidators(typeof(AppHost).Assembly);
+
+            RewriteReturnVoidToNoContent();
+        }
+
+        private void RewriteReturnVoidToNoContent()
+        {
+            this.GlobalResponseFilters.Add((req, res, dto) =>
+            {
+                var returnVoid = req.Dto as IReturnVoid;
+                if (returnVoid == null)
+                    return;
+
+                if (dto == null && res.StatusCode == 200)
+                {
+                    res.StatusCode = (int)HttpStatusCode.NoContent;
+                    res.StatusDescription = "No Content";
+                }
+            });
+        }
+    }
+}
---
+++ src/Evictus/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DrunkenWookie.Evictus;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace DrunkenWookie.Evictus
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            AppHost.Start();
+        }
+    }
+}
